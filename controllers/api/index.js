0e1eb971b26fe5662d80a96d9c79332c22b81aed
Ben Parks <benparks93@gmail.com>
2024-06-06 21:37:54 -0500

    Added ability for employees to add/delete pets, update pet info, and delete customers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,13 @@
 // Notes: This file is used to collect all of the API routes and package them up for the server to use. The index.js file is used to gather up the individual API routes and package them up as a single API object. This file is then imported into the server.js file and used as the single point of access for all API routes. This is done to help with code organization and to make it easier to maintain the application.
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const employeeRoutes = require('./employeeRoutes');
+// const petRoutes = require('./petRoutes');
 
 router.use('/users', userRoutes);
+router.use('/employees', employeeRoutes);
+// router.use('/pets', petRoutes);
+
 
 module.exports = router;
 
