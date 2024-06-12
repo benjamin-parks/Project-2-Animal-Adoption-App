604741c2bb5f7180e2cd941c2362cc34a33f1a5f
alarrabee <alexlarrabee10@gmail.com>
2024-06-11 20:56:06 -0500

    create add new animal form page

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -22,17 +22,36 @@ router.get('/about-us', async (req, res) => {
 });
 
 
+//renders all animals available for adoption on the /allpets page
 router.get("/allpets", async (req, res) => {
   try {
   let petData = await Pet.findAll();
   petData = petData.map((pets) => pets.get({plain:true}));
-  res.render("search", {
+  res.render("allpets", {
     petsArray:petData
   });
 }catch(err) {
   res.status(500).send("Page Not Found");
 }
 });
+
+
+//renders add new animal form
+router.get("/newpet", async (req, res) => {
+  try {
+    res.render("newpet");
+  }
+  catch(err) {
+    res.status(500).send("Form Not Found");
+  }
+});
+
+
+router.post("/api/newpet", async(req, res) => {
+  //console.log(req.body);
+  const petData = await Pet.create(req.body);
+  res.status(200).json(petData);
+})
 
 
 module.exports = router;
