4fae70981fdb30d27e6a9486c74f4391aaddac68
alarrabee <alexlarrabee10@gmail.com>
2024-06-11 19:35:34 -0500

    set up route to adoptable animals page to render all available pets in database with handlebars layouts

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const path = require('path');
 
+const Pet = require('../../models/pet');
+
 router.get('/', async (req, res) => {
   try {
     res.sendFile(path.join(__dirname, '../../public/home.html'));
@@ -17,6 +19,19 @@ router.get('/about-us', async (req, res) => {
   catch (err) {
     res.status(500).json(err);
   }
+});
+
+
+router.get("/allpets", async (req, res) => {
+  try {
+  let petData = await Pet.findAll();
+  petData = petData.map((pets) => pets.get({plain:true}));
+  res.render("search", {
+    petsArray:petData
+  });
+}catch(err) {
+  res.status(500).send("Page Not Found");
+}
 });
 
 
